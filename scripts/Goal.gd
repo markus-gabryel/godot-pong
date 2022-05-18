3228bab6e729c137c050d6093e1263032635c715
Leandro Santiago <lsds.snf20@uea.edu.br>
2022-05-18 02:06:29 -0400

    Added score system

--- scripts/Goal.gd
+++ scripts/Goal.gd
@@ -3,9 +3,13 @@ extends Area2D
 signal player_won
 
 export var player := "Player0"
+export var player_score = 0
+export var text = ""
 
 func _on_Goal_body_entered(body):
 	if body.name == "Ball":
 		# emit_signal("player_won", player)
 		body.position = Vector2(512,300)
 		body.change_direction()
+		player_score += 1
+		print(player," ", player_score, " points")
